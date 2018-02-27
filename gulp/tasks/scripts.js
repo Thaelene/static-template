00bd80727bfd554e73b8aa25148492009aef7efe
thaelene <helene.ta@hetic.net>
2018-02-27 01:08:23 +0100

    Fix scripts eslint

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,30 +1,30 @@
 /* eslint-disable */
-import gulp            from 'gulp'
-import gutil           from 'gulp-util'
-import gulp_uglify     from 'gulp-uglify'
-import browserSync     from 'browser-sync'
-import gulp_sourcemaps from 'gulp-sourcemaps'
-import gulp_notify     from 'gulp-notify'
-import browserify      from 'browserify'
-import babelify        from 'babelify'
-import gulp_rename     from 'gulp-rename'
-import source          from 'vinyl-source-stream'
-import buffer          from 'vinyl-buffer'
-import es2015          from 'babel-preset-env'
-import { scripts }     from '../config'
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import gulp_uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+import gulp_sourcemaps from 'gulp-sourcemaps';
+import gulp_notify from 'gulp-notify';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import gulp_rename from 'gulp-rename';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import es2015 from 'babel-preset-env';
+import { scripts } from '../config';
 /* eslint-enable */
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const scriptsTask = () => {
-  return browserify(scripts.src, { debug: true })
+const scriptsTask = done => {
+  browserify(scripts.src, { debug: true })
     .transform(babelify, { presets: [es2015] })
     .bundle()
     .on(
       'error',
-      gulp_notify.onError(error => {
-        return 'Message to the notifier: ' + error.message;
-      })
+      gulp_notify.onError(error => ({
+        return: 'Message to the notifier: ' + error.message
+      }))
     )
     .pipe(source('main.js'))
     .pipe(buffer())
@@ -35,6 +35,7 @@ const scriptsTask = () => {
     .pipe(gulp.dest(scripts.dest))
     .pipe(browserSync.stream())
     .pipe(gulp_notify('JS compiled'));
+  done();
 };
 
 gulp.task('scripts', scriptsTask);
